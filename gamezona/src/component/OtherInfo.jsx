7ee1e225f5e593ce2a176a90c3d5e65fec45a0e8
nucikk <kirianuci@gmail.com>
2023-11-16 03:00:40 +0400

    fig bug

--- gamezona/src/component/OtherInfo.jsx
+++ gamezona/src/component/OtherInfo.jsx
@@ -4,12 +4,13 @@ function OtherInfo({ formData, setFormData }) {
   return (
     <>
       <div className="sign-info">
-        <input type="text" placeholder="Enter your nationality"
+        <input type="text" placeholder="Enter your Nationality"
           value={formData.nationality}
           onChange={(e) =>
             setFormData({ ...formData, nationality: e.target.value })
           }
         />
+
         <input type="number" placeholder="Enter your age"
           value={formData.age}
           onChange={(e) =>
